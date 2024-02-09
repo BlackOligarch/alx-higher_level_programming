c9ae4564de5f008e4633921c1f8e3092894f1e08
BlackOligarch <godfred.hammond2018@yahoo.com>
2024-02-09 14:47:10 +0000

    Added the first Base Class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,160 +1,273 @@
 #!/usr/bin/python3
-"""Tests the Base class in the models package."""
+"""Defines the base class for other classes."""
 
+import csv
+import os
 import json
-import inspect
-import unittest
-from models.base import Base
-from models.rectangle import Rectangle
+import turtle
 
 
-class TestDocumentation(unittest.TestCase):
-    """Tests the documentation for modules, classes and methods."""
+class Base:
+    """Base object for future classes"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None) -> None:
+        """
+        Initializes a Base object.
+
+        Args:
+            id (int, optional): The ID of the object. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries: "list[dict]") -> str:
+        """
+        Returns the JSON string representation of a list of dictionaries.
+
+        Args:
+            list_dictionaries (list[dict]): The list of dictionaries.
+
+        Raises:
+            TypeError: If the argument received `list_dictionaries` is not a
+            list or does not contain a list of dictionaries.
+
+        Returns:
+            str: The JSON string representation of objects in the
+            `list_dictionaries` else "[]" if empty.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+
+        if type(list_dictionaries) is not list:
+            raise TypeError("list_dictionaries must be a list of dictionaries")
+
+        for obj in list_dictionaries:
+            if type(obj) is not dict:
+                raise TypeError(
+                    "list_dictionaries must be a list of dictionaries"
+                )
+
+        return json.dumps(list_dictionaries)
 
     @classmethod
-    def setUpClass(cls) -> None:
+    def save_to_file(cls, list_objs: list) -> None:
         """
-        Set up class method for the doc tests.
+        Writes the JSON string representation of a list of objects to a file.
+
+        `list_objs` is a list of instances who inherits from the `Base` class.
+        This function uses the concept of polymorphism so it allows for a list
+        of mixed objects that inherits from `Base`.
+
+        Args:
+            list_objs (list): The list of objects that inherits from `Base`.
+
+        Raises:
+            TypeError: If any of the objects in `list_objs` is not an instance
+            of the `Base` class.
         """
-        cls.setup = inspect.getmembers(Base, inspect.isfunction)
+        if list_objs is None or len(list_objs) == 0:
+            with open(f"{cls.__name__}.json", "w") as json_file:
+                json_file.write("[]")
 
-    def test_module_docstring_exists(self):
+            return
+
+        if cls.__name__ not in ["Rectangle", "Square"]:
+            raise TypeError("invalid object type")
+
+        try:
+            with open(f"{cls.__name__}.json", "w") as json_file:
+                list_dictionaries = [obj.to_dictionary() for obj in list_objs]
+                json_file.write(cls.to_json_string(list_dictionaries))
+        except FileNotFoundError:
+            pass
+
+    @staticmethod
+    def from_json_string(json_string: str) -> list:
         """
-        Tests if module docstring documentation exists.
+        Returns the JSON representation of a JSON-formatted string.
+
+        Args:
+            json_string (str): The JSON-formatted string representation.
+
+        Returns:
+            list: A list of JSON representation of `json_string`.
         """
-        self.assertIsNotNone(Base.__doc__)
+        if json_string is None:
+            return []
 
-    def test_classes_docstring_exists(self):
+        if type(json_string) is not str:
+            raise TypeError("json_string must be a string")
+
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary: dict):
         """
-        Tests if class docstring documentation exists.
+        Returns a Rectangle instance with all attributes already set.
+
+        Returns:
+            Rectangle: An instance with all of its attributes already
+            set using `dictionary`.
         """
-        self.assertIsNotNone(Base.__doc__.__class__)
+        dummy = None
 
-    def test_methods_docstring_exists(self):
+        if cls.__name__ == "Rectangle":
+            dummy = cls(4, 8)  # create a Rectangle object
+        elif cls.__name__ == "Square":
+            dummy = cls(8)  # create a Square object
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls) -> list:
         """
-        Tests if methods docstring documentation exists
+        Returns a list of instances from a JSON file.
+
+        Returns:
+            list: A list of instances from a JSON file.
         """
-        for _, method in self.setup:
-            self.assertIsNotNone(method.__doc__)
+        instances = []
+        try:
+            if os.path.exists(f"{cls.__name__}.json"):
+                with open(
+                    f"{cls.__name__}.json", "r", encoding="utf-8"
+                ) as json_file:
+                    content = json_file.read()
 
+                list_dictionaries = cls.from_json_string(content)
+                for dictionary in list_dictionaries:
+                    instances.append(cls.create(**dictionary))
+        except FileNotFoundError:
+            return []
 
-class TestBaseClass(unittest.TestCase):
-    """Tests the Base class."""
+        return instances
 
-    b1 = Base()
-    b2 = Base()
-    b3 = Base(45)
-    b4 = Base(12)
-    b5 = Base()
-    b6 = Base(98)
-
-    def test_automatic_id(self) -> None:
-        """Tests the ID when it is not provided (Generated automatically)."""
-        self.assertEqual(self.b1.id, 1)
-        self.assertEqual(self.b2.id, 2)
-        self.assertEqual(self.b5.id, 3)
-
-    def test_manual_id(self) -> None:
-        """Tests when the ID is provided by the user."""
-        self.assertEqual(self.b3.id, 45)
-        self.assertEqual(self.b4.id, 12)
-        self.assertEqual(self.b6.id, 98)
-
-    def test_automatic_and_manual(self) -> None:
-        """Tests cases when the ID is provided and when it is not."""
-        self.assertEqual(self.b1.id, 1)
-        self.assertEqual(self.b2.id, 2)
-        self.assertEqual(self.b3.id, 45)
-        self.assertEqual(self.b4.id, 12)
-        self.assertEqual(self.b5.id, 3)
-        self.assertEqual(self.b6.id, 98)
-
-    def test_valid_to_json_string(self) -> None:
-        r1 = Rectangle(10, 7, 2, 8)
-        dictionary = r1.to_dictionary()
-        json_dictionary = Base.to_json_string([dictionary])
-        expected_result = json.dumps([dictionary])
-
-        self.assertEqual(json_dictionary, expected_result)
-
-    def test_multi_valid_to_json_string(self) -> None:
-        r1 = Rectangle(10, 7, 2, 8)
-        r2 = Rectangle(3, 6, 4, 1)
-        dictionary_1 = r1.to_dictionary()
-        dictionary_2 = r2.to_dictionary()
-
-        json_dictionary = Base.to_json_string([dictionary_1, dictionary_2])
-        expected_result = json.dumps([dictionary_1, dictionary_2])
-
-        self.assertEqual(json_dictionary, expected_result)
-
-    def test_empty_list_to_json_string(self) -> None:
-        self.assertEqual(Base.to_json_string([]), "[]")
-
-    def test_none_arg_to_json_string(self) -> None:
-        self.assertEqual(Base.to_json_string(None), "[]")
-
-    def test_not_list_arg_to_json_string(self) -> None:
+    @classmethod
+    def save_to_file_csv(cls, list_objs: list) -> None:
         """
-        Tests the `to_json_string` static method using invalid arguments of
-        pass a list.
+        Writes the CSV string representation of a list of objects to a file.
+
+        Args:
+            list_objs (list): The list of objects that inherits from `Base`.
+
+        Raises:
+            TypeError: If any of the objects in `list_objs` is not an instance
+            of the `Base` class.
         """
-        with self.assertRaises(TypeError) as err:
-            Base.to_json_string("not list")
-            (Base.to_json_string(89))
-            Base.to_json_string({"name": "Bob"})
+        with open(f"{cls.__name__}.csv", "w") as csv_file:
+            if list_objs is None or len(list_objs) == 0:
+                csv_file.write("[]")
+                return
 
-        self.assertEqual(
-            err.exception.__str__(),
-            "list_dictionaries must be a list of dictionaries",
-        )
+            if cls.__name__ == "Rectangle":
+                fieldnames = ["id", "width", "height", "x", "y"]
+            elif cls.__name__ == "Square":
+                fieldnames = ["id", "size", "x", "y"]
+            elif not issubclass(cls, Base):
+                raise TypeError("incompatible object type")
 
-    def test_not_list_of_dict_arg_to_json_string(self) -> None:
-        with self.assertRaises(TypeError) as err:
-            Base.to_json_string([4, "8", (1, 2, 3)])
-            (Base.to_json_string([[1, 2, 3], {4, 5, 6}]))
-            Base.to_json_string([{"name": "Bob"}, "something else"])
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            for obj in list_objs:
+                writer.writerow(obj.to_dictionary())
 
-        self.assertEqual(
-            err.exception.__str__(),
-            "list_dictionaries must be a list of dictionaries",
-        )
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        Returns a list of instances from a CSV file.
 
-    def test_no_args_to_json_string(self) -> None:
-        exception_msg = (
-            "missing 1 required positional argument: 'list_dictionaries'"
-        )
+        Returns:
+            list: A list of instances from a CSV file.
+        """
+        instances = []
+        try:
+            file_name = f"{cls.__name__}.csv"
+            if os.path.exists(file_name):
+                with open(file_name, "r", newline="") as csv_file:
+                    rows = []
+                    if cls.__name__ == "Rectangle":
+                        fieldnames = ["id", "width", "height", "x", "y"]
+                    elif cls.__name__ == "Square":
+                        fieldnames = ["id", "size", "x", "y"]
+                    elif not issubclass(cls, Base):
+                        raise TypeError("Incompatible object type")
 
-        with self.assertRaisesRegex(TypeError, exception_msg):
-            Base.to_json_string()
+                    reader = csv.DictReader(csv_file)
+                    for row in reader:
+                        for data in row:
+                            row[data] = int(row[data])
+                        rows.append(row)
 
+                    instances = [cls.create(**obj) for obj in rows]
+        except FileNotFoundError:
+            return []
 
-class TestFromJsonStringBase(unittest.TestCase):
-    """Tests the `from_json_string() static method on Base."""
+        return instances
 
-    def test_from_json_to_string_none_arg(self) -> None:
-        """Tests for empty lists if None is passed as argument."""
-        self.assertEqual(Base.from_json_string(None), [])
+    @classmethod
+    def draw(cls, list_rectangles, list_squares) -> None:
+        """Draw rectangles and squares side by side using the turtle module.
 
-    def test_from_json_string_empty_list(self) -> None:
-        """Tests for if an empty list is passed as argument."""
-        self.assertEqual(Base.from_json_string("[]"), [])
+        Args:
+            list_rectangles (list): list of Rectangle objects to draw.
+            list_squares (list): list of Square objects to draw.
+        """
+        screen = turtle.Screen()
+        screen.bgcolor("#180302")
+        screen.title("Shapes | BlackOligarch*_*")
 
-    def test_from_json_to_string_non_json_string(self) -> None:
-        """Tests encoding errors for non-JSON strings."""
-        with self.assertRaises(json.decoder.JSONDecodeError):
-            Base.from_json_string("Hello")
+        drawer = turtle.Turtle()
+        drawer.pensize(5)
+        drawer.shape("circle")
+        drawer.speed(5)
 
-    def test_from_json_string_integer_arg(self) -> None:
-        """Tests for TypeError raised if an integer is passed as argument."""
-        with self.assertRaisesRegex(TypeError, "must be a string"):
-            Base.from_json_string(87)
+        cls._draw_shapes(drawer, list_rectangles, "#f4f45d", side_by_side=True)
+        cls._draw_shapes(drawer, list_squares, "#0670d4", side_by_side=True)
 
-    def test_from_json_string_list_arg(self) -> None:
-        """Tests for TypeError raised if a list is passed as argument."""
-        with self.assertRaisesRegex(TypeError, "must be a string"):
-            Base.from_json_string(["Hello"])
+        turtle.exitonclick()
 
-    def test_from_json_string_valid_data(self) -> None:
-        """Tests for valid data passed to `from_json_string_method`."""
-        self.assertEqual(Base.from_json_string('[{"id": 5}]'), [{"id": 5}])
+    @staticmethod
+    def _draw_shapes(turtle_drawer, shapes, color, side_by_side=False) -> None:
+        """Draw a list of shapes using the turtle module.
+
+        Args:
+            turtle_drawer (turtle.Turtle)object: The turtle used for drawing
+            shapes (list): A list of shapes (Rectangle or Square objects) to
+            draw.
+            color (str): The color to use for drawing.
+            side_by_side (bool): Whether to draw shapes side by side.
+        """
+        turtle_drawer.color(color)
+
+        for shape in shapes:
+            turtle_drawer.showturtle()
+            turtle_drawer.up()
+
+            if side_by_side:
+                if shape.__class__.__name__ == "Square":
+                    turtle_drawer.goto(shape.x + shape.size, shape.y)
+                elif shape.__class__.__name__ == "Rectangle":
+                    turtle_drawer.goto(shape.x + shape.width, shape.y)
+            else:
+                turtle_drawer.goto(shape.x, shape.y)
+
+            turtle_drawer.down()
+            if shape.__class__.__name__ == "Square":
+                for _ in range(4):
+                    turtle_drawer.forward(shape.size)
+                    turtle_drawer.left(90)
+            elif shape.__class__.__name__ == "Rectangle":
+                for _ in range(2):
+                    turtle_drawer.forward(shape.width)
+                    turtle_drawer.left(90)
+                    turtle_drawer.forward(shape.height)
+                    turtle_drawer.left(90)
+
+            turtle_drawer.hideturtle()
